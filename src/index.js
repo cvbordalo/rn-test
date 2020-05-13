e65af3524233c08db1db76f40370d640e91e6977
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-05-13 19:26:44 -0300

    test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+  return <View />;
+}
+
+const styles = styleSheet.create({
+
+});
